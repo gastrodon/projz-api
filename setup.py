b130d5d45db972d66a8034177a05d323eeb6e804
Noah Harris <dakoolstwunn@gmail.com>
2020-10-06 01:50:15 -0400

    Create setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = "z",
+    version = "0.1.0",
+    description = "A python interface to the project Z API",
+    author = "Noah Harris",
+    author_email = "mail@gastrodon.io",
+    url = "https://github.com/gastrodon/projz-api",
+    packages = setuptools.find_packages(),
+)
