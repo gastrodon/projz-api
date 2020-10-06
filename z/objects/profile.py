c59222b2826db0317e959b7f6d06df5035557d4a
Noah Harris <dakoolstwunn@gmail.com>
2020-10-06 14:32:03 -0400

    Add profile getters bound to .get

--- z/objects/profile.py
+++ z/objects/profile.py
@@ -7,3 +7,101 @@ class Profile(ZThing):
         super().__init__(**kwargs)
         self.id = id
         self._route = f"/users/profile/{self.id}"
+
+    @property
+    def chat_invitation_status(self) -> int:
+        # TODO: is this a bool?
+        return self.get("chatInvitationStatus")
+
+    @property
+    def content_region(self) -> str:
+        return self.get("contentRegion")
+
+    @property
+    def content_region_name(self) -> str:
+        return self.get("contentRegionName")
+
+    @property
+    def created(self) -> int:
+        return self.get("createdTime")
+
+    @property
+    def fan_count(self) -> int:
+        return self.get("fansCount")
+
+    @property
+    def following_count(self) -> int:
+        return self.get("followingCount")
+
+    @property
+    def friend_count(self) -> int:
+        return self.get("friendsCount")
+
+    @property
+    def status(self) -> int:
+        return self.get("status")
+
+    @property
+    def online_status(self) -> int:
+        return self.get("onlineStatus")
+
+    @property
+    def gender(self) -> str:
+        genders: dict[str, any] = {
+            0: "female",
+            1: "male",
+        }
+        return self.get("gender", casted = lambda it : genders.get(it, "other"))
+
+    @property
+    def icon(self) -> dict[str, any]:
+        # TODO object to represent media
+        return self.get("icon")
+
+    @property
+    def language(self) -> str:
+        return self.get("language")
+
+    @property
+    def location(self) -> dict[str, any]:
+        return self.get("location")
+
+    @property
+    def nickname(self) -> str:
+        return self.get("nickname")
+
+    @property
+    def school(self) -> str:
+        return self.get("school")
+
+    @property
+    def social_id(self) -> str:
+        return self.get("socialId")
+
+    @property
+    def social_id_modified(self) -> int:
+        return self.get("socialIdModified")
+
+    @property
+    def has_profile(self) -> bool:
+        return self.get("hasProfile", casted = bool)
+
+    @property
+    def name_card_enabled(self) -> bool:
+        return self.get("nameCardEnabled", casted = bool)
+
+    @property
+    def push_enabled(self) -> bool:
+        return self.get("pushEnabled", casted = bool)
+
+    @property
+    def shows_joined_circles(self) -> bool:
+        return self.get("showsJoinedCircles", casted = bool)
+
+    @property
+    def shows_location(self) -> bool:
+        return self.get("showsLocation", casted = bool)
+
+    @property
+    def shows_school(self) -> bool:
+        return self.get("showsSchool", casted = bool)
