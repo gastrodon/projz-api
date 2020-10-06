4eefe24623416d26df4d4b7ae27f42939e33317e
Noah Harris <dakoolstwunn@gmail.com>
2020-10-06 14:29:25 -0400

    Add api_code to exceptions
    Add api_code to disambiguate exceptions

--- z/ext/exceptions.py
+++ z/ext/exceptions.py
@@ -1,3 +1,4 @@
 class APIException(Exception):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, api_code: int = 0, **kwargs):
         super().__init__(*args, **kwargs)
+        self.api_code = api_code
